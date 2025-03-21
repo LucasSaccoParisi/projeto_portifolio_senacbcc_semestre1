6ec8b278c40f38bf93b9047ee8a6cd5352593635
LucasSaccoParisi <lucas.sparisi@senacsp.edu.br>
2025-03-21 11:00:02 -0300

    Add files via upload

---
+++ html/script.js
@@ -0,0 +1,16 @@
+function escrever (texto){
+    document.write("texto");
+}
+
+function popUp (texto) {
+    alert("texto");
+}
+
+function relatorio (texto){
+    console.log("texto");
+}
+
+function elemento (xyz) {
+    document.getElementById("xyz");
+}
+
