068042e69c0ca262f18c7ce30feaf83fd666b311
LucasSaccoParisi <lucas.sparisi@senacsp.edu.br>
2025-03-21 11:16:54 -0300

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,27 @@
+function escrever (texto){
+    document.write("texto");
+}
+
+function popUp (texto) {
+    alert("texto");
+}
+
+function relatorio (texto){
+    console.log("texto");
+}
+
+function elemento (xyz) {
+    document.getElementById("xyz");
+}
+
+elemento("playerDeMusic");
+elemento("botaoPlay");
+elemento("botaoPause");
+elemento("botaoStop");
+
+var musica = {
+    nome: "Nightcall",
+    artista: "Kavinsky",
+    caminho: "\html\musicas\nightcall.mp3"
+};
+
